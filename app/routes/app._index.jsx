14c9fabb8e5c5d165111c1a0f57c0ce80c503a40
Jonas Gonzalez <jonasok82@gmail.com>
2026-05-06 07:12:41 -0400

    feat: actualizar video tutorial al nuevo video de Calendify

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -168,7 +168,7 @@ export default function Index() {
               }}>
                 <iframe
                   style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", border: 0 }}
-                  src="https://www.youtube-nocookie.com/embed/GYWJlrS0iIc"
+                  src="https://www.youtube-nocookie.com/embed/58cKckcd52Y"
                   title="Tutorial completo de Calendify Delivery"
                   frameBorder="0"
                   loading="lazy"
